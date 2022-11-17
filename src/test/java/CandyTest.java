9652c3636d177b0d2772793dc6531d48fd69de43
20212327 <20212327@S20212327.campus.tue.nl>
2022-11-17 09:50:03 +0100

    I hope this is semi okay :P

---
+++ src/test/java/CandyTest.java
@@ -0,0 +1,55 @@
+//# BEGIN SKELETON
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * A JUnit test case class.
+ * Every method starting with the word "test" will be called when running
+ * the test with JUnit.
+ *
+<!--//# BEGIN TODO: Name, id, and date-->
+<p><b>Tygo van den Hurk, 1705709, 17/11/2022</b></p>
+<!--//# END TODO-->
+ */
+// -----8<----- cut line -----8<-----
+public class CandyTest {
+
+    static final Candy SUT = null; // to simplify method calls
+
+    static final long MAX_VALUE = 999999999999999999L;
+
+    /**
+     * Checks the result of SUT.divide(k, c).
+     */
+    private void check(long k, long c, boolean expected) {
+        System.out.println("divide(" + k + ", " + c + ")");
+        long result = SUT.divide(k, c);
+        System.out.println("  result = " + result);
+        assertEquals(expected, 0 <= result, "possible (0 <= result)");
+        if (0 <= result) {
+            assertTrue(result <= MAX_VALUE, "range (result <= MAX_VALUE)");
+            assertEquals(result * k, c, "quotient (result * k == c)");
+        }
+    }
+
+    // Test cases
+
+    /** The given example. */
+    @Test
+    public void testDivideGivenExample() {
+        check(3, 15, true);
+    }
+
+//# BEGIN TODO: Additional test cases
+    @Test
+    public void testDivide() {
+        check(3, 15, true);
+        check(0, 15, false);
+        check(4, 16, true);
+        check(4, 0, true);
+
+    }
+//# END TODO
+
+}
+//# END SKELETON
